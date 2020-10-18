3af21b43af10c6cc83f766469859911085cc04cb
homeless-peep <spirosmakris92@gmail.com>
2020-10-18 15:54:35 +0300

    merged into .h file

--- button.h
+++ button.h
@@ -8,18 +8,58 @@
 #define B_OFF 0
 class Button
 {
-public:
-    Button();
-    Button(volatile uint8_t *PORT, uint8_t PIN);
-    ~Button();
+	public:
+	Button()
+	{
+		state = 0;
+		PORT = 0;
+		PIN = 0;
+	}
+	
+	Button(volatile uint8_t *PORT, uint8_t PIN)
+	{
+		this->PORT = PORT;
+		this->PIN = PIN;
+	}
+	~Button()
+	{
+		
+	}
 
-    uint8_t update();
-    uint8_t read(uint8_t do_read = 0);
+	uint8_t update()
+	{
+		volatile uint16_t temp = *PORT;
+		state = (state << 1) | (!((temp & (1 << PIN)) >> PIN));
+		// state &= 0b11111111;
+		return state;
+	}
+	uint8_t read(uint8_t do_read = 0)
+	{
+		if (do_read == 1)
+		{
+			update();
+		}
+		switch (state)
+		{
+			case (0xFF>>1):
+			return B_TRIG;
+			break;
+			case 0xFF:
+			return B_HOLD;
+			break;
+			case (0xFF<<1):
+			return B_REL;
+			break;
+			default:
+			return B_OFF;
+			break;
+		}
+	}
 
-private:
-    uint16_t state;
-    volatile uint8_t *PORT;
-    uint16_t PIN;
+	private:
+	uint16_t state;
+	volatile uint8_t *PORT;
+	uint16_t PIN;
 };
 
 #endif
